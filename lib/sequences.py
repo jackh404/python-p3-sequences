8044a51b465d98c124548fe1d42f984c0e1c6145
Jack Henza <jackhenza@gmail.com>
2023-11-17 15:29:13 -0500

    yay

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    fibList = [0]
+    fibNum = 1
+    for i in range(length-1):
+        fibList.append(fibNum)
+        fibNum = fibList[i] + fibNum
+    print(fibList)
+
+print_fibonacci(9)
